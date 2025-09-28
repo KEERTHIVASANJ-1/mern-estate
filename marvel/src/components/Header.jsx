396a4c53254082e39ec132ba01389830d9861984
Keerthivasan <keerthivasanj.ec23@bitsathy.ac.in>
2025-09-28 16:36:51 +0530

    db connected+made express backend

--- marvel/src/components/Header.jsx
+++ marvel/src/components/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
               <Link to="/about">
                 <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
               </Link>
-              <Link to="sign In">
+              <Link to="sign-in">
                 <li className="text-slate-700 hover:underline">Sign In</li>
               </Link>
             </ul>
